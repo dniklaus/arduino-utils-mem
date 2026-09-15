585f921afcc986030141b1738b6e5db1c8323fca
Dieter Niklaus <dieter.niklaus@gmx.net>
2026-09-15 14:34:26 +0200

    NEW: support STM32F4xx

--- RamUtils.cpp
+++ RamUtils.cpp
@@ -13,6 +13,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 extern char* _sbrk(int i);
+#elif defined (__arm__) && defined (STM32F411xE) // STM32F4xx
+#include <sys/types.h>
+#include <errno.h>
+
+// Deklaration des Heap-Endes aus dem Linker-Skript
+extern uint8_t _end; 
+
+caddr_t _sbrk(int incr) {
+    static uint8_t *heap_end = NULL;
+    uint8_t *prev_heap_end;
+
+    // Beim ersten Aufruf den Heap-Start setzen
+    if (heap_end == NULL) {
+        heap_end = &_end;
+    }
+
+    prev_heap_end = heap_end;
+
+    // optional: Hier könnte ein Check gegen das Stack-Ende stehen (Stack-Overflow-Schutz)
+
+    heap_end += incr;
+    return (caddr_t) prev_heap_end;
+}
 #endif
 
 #if defined (ARDUINO_ARCH_SAMD) && defined (__SAMD21G18A__) // Adafruit Feather M0
@@ -36,6 +59,9 @@ int RamUtils::getFreeRam()
   char* heapend = _sbrk(0);
   register char * stack_ptr asm("sp");
   free_memory = stack_ptr - heapend + mi.fordblks;
+#elif defined (__arm__) && defined (STM32F411xE) // STM32F4xx
+  char stack_dummy = 0;
+  free_memory = &stack_dummy - _sbrk(0);
 #elif defined (ARDUINO_ARCH_SAMD) && defined (__SAMD21G18A__) // Adafruit Feather M0
   char stack_dummy = 0;
   free_memory = &stack_dummy - sbrk(0);
